d626c22075c1a439a70a0785ce758d1eed7b51c3
何良平 <lmheliangping@163.com>
2019-10-09 22:57:42 +0800

    头部完成

--- shop-app/src/main.js
+++ shop-app/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//全局引入axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios)
+
 import 'swiper/dist/css/swiper.css'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 Vue.use(VueAwesomeSwiper)
@@ -10,6 +16,15 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 Vue.config.productionTip = false
+
+//按需引入vant组件
+import {NavBar,Grid, GridItem,Tab, Tabs } from 'vant';
+import 'vant/lib/index.css'
+Vue.use(NavBar).use(Grid).use(GridItem).use(Tab).use(Tabs);
+// import 'vant/lib/icon/local.css';
+
+//引入公用样式
+import './assets/css/public.css'
 
 new Vue({
   router:router,
